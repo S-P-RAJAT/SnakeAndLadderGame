57dcb997ce4f36400b81a3b35a1d86cd8c27b8e3
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-21 18:50:58 +0530

    ADD: Add Rolling Dice feature

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,15 +2,24 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
 	int position;
+	String name;
 	
-	public SnakeAndLadder() {
+	public SnakeAndLadder(String name) {
 		this.position = 0;
+		this.name = name;
 	}
-	
-	
+	public static int rollDice() {
+		return ((int)(Math.floor(Math.random()*10)%6)+1);
+	}
+	public void play() {
+		int result = rollDice();
+		System.out.println(this.name+": Rolling Dice Value = "+result);
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game!");
 		System.out.println("");
-		SnakeAndLadder player1 = new SnakeAndLadder();
+		SnakeAndLadder player1 = new SnakeAndLadder("Player 1");
+		player1.play();
+		
 	}
 }
