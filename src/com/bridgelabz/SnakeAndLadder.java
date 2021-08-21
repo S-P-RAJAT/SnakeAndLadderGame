1be758c876a667c8d516fd7ee716b7632b9aa893
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-21 18:52:03 +0530

    ADD: ADD option for NOPLAY,LADDER and SNAKE

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,6 +3,9 @@ package com.bridgelabz;
 public class SnakeAndLadder {
 	int position;
 	String name;
+	public static final int NOPLAY = 0;
+	public static final int LADDER = 1;
+	public static final int SNAKE = 2;
 	
 	public SnakeAndLadder(String name) {
 		this.position = 0;
@@ -14,6 +17,27 @@ public class SnakeAndLadder {
 	public void play() {
 		int result = rollDice();
 		System.out.println(this.name+": Rolling Dice Value = "+result);
+		
+		int option = ((int)(Math.floor(Math.random()*10)%3));
+		switch (option) {
+		case NOPLAY:
+			result = 0;
+			System.out.println("OPTION: NOPLAY");
+			break;
+		case LADDER:
+			System.out.println("OPTION: LADDER");
+
+			break;
+		case SNAKE:
+			System.out.println("OPTION: SNAKE");
+			result = -result;
+			break;
+
+		default: System.out.println("Invalid Option!");
+			break;
+		}
+		position += result;
+		System.out.println(position);
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game!");
